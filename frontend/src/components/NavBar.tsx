fc5d9a9fcd4e96a6732683df0714b3cdec72a6ca
Sanjeev-Kumar78 <sanjeev-kumar78@outlook.com>
2026-01-15 21:23:27 +0530

    feat: update NavBar component and styles; add Login page with image carousel and form

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,15 +1,24 @@
 import { Link } from "react-router";
+import "../styles/NavBar.css";
 
 const NavBar = () => {
-    return (
-        <nav>
-            <ul>
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/about">About</Link>
-                <Link to="/contact">Contact</Link>
-            </ul>
-        </nav>
-    );
-}
+  return (
+    <nav className="w-[100vw] h-16 bg-blue-300 flex flex-row">
+      <ul className="flex flex-row items-center p-5">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 export default NavBar;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,13 @@
+import NavBar from "../components/NavBar";
+
 const HomePage: React.FC = () => {
-    return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-            <p>This is the main landing page of the application.</p>
-        </div>
-    );
-}
+  return (
+    <div>
+      <NavBar />
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main landing page of the application.</p>
+    </div>
+  );
+};
 
 export default HomePage;
